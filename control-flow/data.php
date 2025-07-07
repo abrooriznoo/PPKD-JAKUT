8d30b3433f608fed1c4ff2f85e620749f887bdce
Abroor Rizky <abroorrizky@gmail.com>
2025-07-07 13:57:58 +0700

    update learning

---
+++ control-flow/data.php
@@ -0,0 +1,31 @@
+<?php
+$data = [
+    [
+        'name' => 'Abroor Rizky',
+        'age' => 23,
+        'city' => 'Cempaka Putih'
+    ],
+    [
+        'name' => 'Reiji',
+        'age' => 24,
+        'city' => 'Kemayoran'
+    ],
+    [
+        'name' => 'Raka',
+        'age' => 27,
+        'city' => 'Tebet'
+    ],
+    [
+        'name' => 'Sandi',
+        'age' => 26,
+        'city' => 'Cipayung'
+    ],
+    [
+        'name' => 'Dio',
+        'age' => 25,
+        'city' => 'Bintaro'
+    ],
+];
+
+// print_r($data);
+// die();
--- control-flow/index.php
+++ control-flow/index.php
@@ -299,8 +299,8 @@
             <hr class="mb-4" style="margin: 0 auto; border: 2px solidrgb(0, 0, 0); border-radius: 5px;">
             <div class="d-flex justify-content-center">
                 <div class="card-body mb-3" style="width: 95%; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-                    <h4 class="text-start fst-italic">Array Satu Dimensi</h4>
-                    <hr class="mb-4" style="margin: 0 auto; border: 2px solidrgb(0, 0, 0); border-radius: 5px;">
+                    <!-- <h4 class="text-start fst-italic">Array Satu Dimensi</h4> -->
+                    <!-- <hr class="mb-4" style="margin: 0 auto; border: 2px solidrgb(0, 0, 0); border-radius: 5px;"> -->
                     <div class="table-responsive">
                         <table class="table table-striped">
                             <thead>
@@ -350,15 +350,71 @@
                     </div>
                 </div>
             </div>
+            <div class="card-body mb-3" style="width: 100%; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                <h4 class="text-start fst-italic">Function GET</h4>
+                <hr class="mb-4" style="margin: 0 auto; border: 2px solidrgb(0, 0, 0); border-radius: 5px;">
+                <div>
+                    <div class="mb-3">
+                        <form action="" method="get">
+                            <input type="text" name="name" placeholder="Nama" class="form-control mb-2" style="width: 30%; display: inline-block;">
+                            <button type="submit" class="btn btn-primary">Cari</button>
+                        </form>
+                    </div>
+                    <?php
+                    include "data.php";
+
+                    $peserta = $data; // Ambil semua data dari file data.php
+                    $allPeserta = $peserta; // Simpan array asli untuk pencarian index
+
+                    if (!empty($_GET)) {
+                        $name = $_GET['name'];
+                        $peserta = array_filter($peserta, function ($p) use ($name, $allPeserta) {
+                            if (stripos($p['name'], $name) !== false) {
+                                $index = array_search($p, $allPeserta, true);
+                                // echo "Data Ditemukan dengan Nilai Array ke-{$index} <br>";
+                                return true;
+                            }
+                            return false;
+                        });
+                    }
+
+                    ?>
+                    <div class="card-body" style="border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                        <table class="table table-bordered" style="width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Nama</th>
+                                    <th>Umur</th>
+                                    <th>Alamat</th>
+                                </tr>
+                            </thead>
+                            <?php
+                            $no = 1;
+                            foreach ($peserta as $p) {
+                            ?>
+                                <tbody>
+                                    <tr>
+                                        <td><?= $no++ ?>.</td>
+                                        <td><?= $p['name'] ?></td>
+                                        <td><?= $p['age'] ?></td>
+                                        <td><?= $p['city'] ?></td>
+                                    </tr>
+                                </tbody>
+                            <?php } ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
-        <!-- <script>
+    <!-- <script>
         const table = document.getElementById("countingTable");
         const rows = table.getElementsByTagName("tr");
     </script> -->
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
